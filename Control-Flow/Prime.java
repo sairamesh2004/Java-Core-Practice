1cfb3a69ed3339375754d3f07e49befb51501656
Sai Ramesh Vajjala <145277796+sairamesh2004@users.noreply.github.com>
2026-06-24 15:55:32 +0530

    Revise Prime class for enhanced prime detection
    Refactor Prime class to improve prime number checking logic and user interaction.

--- Control-Flow/Prime.java
+++ Control-Flow/Prime.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
+
 class Prime
 {
-Scanner sc=new Scanner(System.in);
-int a;
-void task()
-{
-System.out.println("Enter a value");
-a=sc.nextInt();
-if(a%2==1)
-{
-System.out.println(a+"Is a Prime Number");
-}
-else {
-System.out.println(a+"Is not a Prime Number");
-}
-}
-public static void main(String[] args)
-{
-Prime p=new Prime();
-p.task();
-}
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
 
+        int isPrime = 1;
+        int i;
+
+        if (num <= 1)
+        {
+            isPrime = 0;
+        }
+        else
+        {
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = 0;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime == 1)
+        {
+            System.out.println(num + " is a Prime Number");
+        }
+        else
+        {
+            System.out.println(num + " is Not a Prime Number");
+        }
+    }
 }
