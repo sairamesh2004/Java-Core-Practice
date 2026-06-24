ee3ff075d79a40a084ef4fd5fa1c2e516a503641
Sai Ramesh Vajjala <145277796+sairamesh2004@users.noreply.github.com>
2026-06-24 13:57:00 +0530

    Add PrintingName class to print full name

---
+++ Basics/PrintingName.java
@@ -0,0 +1,18 @@
+class PrintingName
+{
+   String firstname="Sai";
+   String middlename="Ramesh";
+   String lastname="Vajjala";
+   
+  void print(){
+    System.out.println("My full Name is "+firstname+" "+middlename+" "+lastname);
+
+   }
+     public static void main(String[] args){
+       PrintingName pn=new PrintingName();
+        pn.print();
+
+
+}
+
+}
